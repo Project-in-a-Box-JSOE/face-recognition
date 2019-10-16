fc08e6dcae7a1b54ac46276d9b87d69abf8cbd26
Kenmei <1996brianhnenriquez@gmail.com>
2019-10-16 14:54:45 -0700

    updated install

--- setup/rpi/install_opencv2.sh
+++ setup/rpi/install_opencv2.sh
@@ -28,13 +28,7 @@ sudo apt-get install libdc1394-22-dev -y
    
 # Python and Numpy
 sudo apt-get install python-dev python-numpy -y
+sudo apt-get install libqt4-test libqtgui4 liblapack-dev libatlas-base-dev -y
 
-#Get files
-wget -O opencv.zip https://github.com/opencv/opencv/archive/2.4.13.4.zip
-unzip opencv.zip
-cd /home/pi/opencv-2.4.13.4
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
-make -j3            # May take up to 2 hours.
-sudo make install
+# Use Python3!
+pip3 install keras opencv-python flask flask_cors
